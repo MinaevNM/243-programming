cf6576b81d40515b965bd5d74034c7fa882313f9
Nikita <nikita_340@mail.ru>
2014-12-22 15:38:41 +0300

    Update main.cpp

--- 1.2/main.cpp
+++ 1.2/main.cpp
@@ -1,88 +1,85 @@
-#include <iostream>
-#include <queue>
-#include <map>
-#include <string>
+#include <iostream> 
+#include <queue> 
+#include <map> 
+#include <string> 
+#include <cmath>
 
-#include "tree.h"
+#include "tree.h" 
 
-#pragma warning(disable: 4996)
+#pragma warning(disable: 4996) 
 
-using namespace std;
+using namespace std; 
 
-class mycomparison
-{
-	bool reverse;
+class mycomparison 
+{ 
+public: 
+	mycomparison() 
+    { 
+	} 
+	 
+	bool operator() ( Tree & lhs, Tree & rhs ) const 
+	{ 
+		return (lhs.getRootValue().second > rhs.getRootValue().second); 
+	} 
+}; 
 
-public:
-	mycomparison( const bool & revparam = false )
-    {
-		reverse = revparam;
-	}
-	
-	bool operator() ( Tree & lhs, Tree & rhs ) const
-	{
-		if (reverse) 
-			return (lhs.getRootValue().second < rhs.getRootValue().second);
-		else
-			return (lhs.getRootValue().second > rhs.getRootValue().second);
-	}
-};
-
-void main()
-{
-	freopen("input.txt", "rt", stdin);
-	
-	int num = 0;
-	char c;
-	map<char, int> letters;
+void main() 
+{ 
+	freopen("input.txt", "rt", stdin); 
+	 
+	int num = 0; 
+	int diff_letters;
+	char c; 
+	map<char, int> letters; 
 	map<char, int>::iterator it;
 	
-	while ((c = getchar()) > 0)
-	{
-		num++;
-		it = letters.find(c);
-		if (it == letters.end())
-			letters[c] = 1;
-		else
-		{
-			letters[c]++;
-		}
-	}
+	while ((c = getchar()) > 0) 
+	{ 
+		num++; 
+		it = letters.find(c); 
+		if (it == letters.end()) 
+			letters[c] = 1; 
+		else 
+		{ 
+			letters[c]++; 
+		} 
+	} 
 
-	priority_queue<Tree, vector<Tree>, mycomparison> pq;
+	diff_letters = letters.size();
 
-	for (it = letters.begin(); it != letters.end(); it++)
-	{
-		Tree t(it->first, it->second);
-		pq.push(t);
-	}
+ 	priority_queue<Tree, vector<Tree>, mycomparison> pq; 
 
-	while (pq.size() != 1)
-	{
-		Tree t1 = pq.top();
-		pq.pop();
-		Tree t2 = pq.top();
-		pq.pop();
+	for (it = letters.begin(); it != letters.end(); it++) 
+	{ 
+		Tree t(it->first, it->second); 
+		pq.push(t); 
+	} 
 
-		Tree t(t1, t2);
-		pq.push(t);
-	}
+	while (pq.size() != 1) 
+	{ 
+		Tree t1 = pq.top(); 
+		pq.pop(); 
+		Tree t2 = pq.top(); 
+		pq.pop(); 
 
-	Tree t = pq.top();
-	map<char, string> * codes = t.walk();
+		Tree t(t1, t2); 
+		pq.push(t); 
+	} 
 
-	int codelength = 0;
-	for (map<char, string>::iterator it = codes->begin(); it != codes->end(); it++)
-		codelength += it->second.length() * letters[it->first];
+	Tree t = pq.top(); 
+	map<char, string> * codes = t.walk(); 
 
-	cout << num << endl << codelength << endl;
-	cout << "compression coeff: " << (num * 8. / codelength) << endl; 
+	int codelength = 0; 
+	for (map<char, string>::iterator it = codes->begin(); it != codes->end(); it++) 
+		codelength += it->second.length() * letters[it->first]; 
 
-	for (map<char, string>::iterator it = codes->begin(); it != codes->end(); it++)
-	{
-		if (it->first == ' ')
-			cout << "space: " << it->second << endl;
-		else
-			cout << it->first << ":     " << it->second << endl;
-	}
+	cout << "compression coeff: " << (ceil(log((double)diff_letters) / log(2.)) * num / codelength) << endl;  
+
+	for (map<char, string>::iterator it = codes->begin(); it != codes->end(); it++) 
+	{ 
+		if (it->first == ' ') 
+			cout << "space: " << it->second << endl; 
+		else 
+			cout << it->first << ":     " << it->second << endl; 
+	} 
 }
