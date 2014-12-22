6cfe4cc6bb43e6c00d0792ab448e1af7751a5c6e
Nikita <nikita_340@mail.ru>
2014-12-22 11:38:09 +0300

    FFT added but not fixed

--- 3.1/main.cpp
+++ 3.1/main.cpp
@@ -1,8 +1,142 @@
 #include <iostream>
+#include <complex>
 
 #pragma warning(disable: 4996)
 
 using namespace std;
+
+const double pi = 3.14159265358979323846;
+
+complex<double> * FFT( complex<double> * polynom, int degree )
+{
+	complex<double> * values = new complex<double>[degree];
+
+	if (degree == 1)
+	{
+		values[0] = polynom[0];
+		return values;
+	}
+
+	complex<double> * roots = new complex<double>[degree];
+	for (int i = 0; i < degree; i++)
+	{
+		double phi = 2 * pi * i / degree;
+		roots[i] = complex<double>(cos(phi), sin(phi));
+	}
+
+	complex<double> * pol1 = new complex<double>[degree / 2 + 1];
+	complex<double> * pol2 = new complex<double>[degree / 2 + 1];
+
+	for (int i = 0; i < degree; i++)
+	{
+		if (!(i % 2))
+			pol1[i / 2] = polynom[i];
+		else
+			pol2[i / 2] = polynom[i];
+	}
+
+	complex<double> * val1 = new complex<double>[degree / 2 + 1];
+	complex<double> * val2 = new complex<double>[degree / 2 + 1];
+
+	val1 = FFT(pol1, degree / 2);
+	val2 = FFT(pol2, degree / 2);
+
+	for (int i = 0; i < degree; i++)
+		values[i] = val1[i % (degree / 2)] + roots[i] * val2[i % (degree / 2)];
+
+	delete [] val1;
+	delete [] val2;
+	delete [] pol1;
+	delete [] pol2;
+	delete [] roots;
+
+	return values;
+}
+
+complex<double> * IFFT( complex<double> * values, int degree )
+{
+	complex<double> * polynom = new complex<double>[degree];
+
+	if (degree == 1)
+	{
+		polynom[0] = values[0];
+		return polynom;
+	}
+
+	complex<double> * val1 = new complex<double>[degree / 2 + 1];
+	complex<double> * val2 = new complex<double>[degree / 2 + 1];
+
+	for (int i = 0; i < degree; i++)
+	{
+		if (!(i % 2))
+			val1[i / 2] = values[i];
+		else
+			val2[i / 2] = values[i];
+	}
+
+	complex<double> * pol1 = new complex<double>[degree / 2 + 1];
+	complex<double> * pol2 = new complex<double>[degree / 2 + 1];
+
+	pol1 = IFFT(val1, degree / 2);
+	pol2 = IFFT(val2, degree / 2);
+
+	double phi = -2 * pi / degree;
+	complex<double> root = complex<double>(1, 0);
+	complex<double> dr = complex<double>(cos(phi), sin(phi));
+
+	for (int i = 0; i < degree / 2; i++)
+	{
+		polynom[i] = (pol1[i] + root * pol2[i]) * 0.5;
+		polynom[i + degree / 2] = (pol1[i] - root * pol2[i]) * 0.5;
+		root *= dr;
+	}
+
+	delete [] val1;
+	delete [] val2;
+	delete [] pol1;
+	delete [] pol2;
+	
+	return polynom;
+}
+
+char * FastMultiply( char * str1, char * str2, int degree )
+{
+	complex<double> * pol1 = new complex<double>[degree];
+	complex<double> * pol2 = new complex<double>[degree];
+	complex<double> * pol3 = new complex<double>[degree];
+
+	for (int i = 0; i < degree; i++)
+	{
+		pol1[i] = complex<double>(str1[i], 0);
+		pol2[i] = complex<double>(str2[i], 0);
+	}
+	
+	complex<double> * val1 = new complex<double>[degree];
+	complex<double> * val2 = new complex<double>[degree];
+	complex<double> * val3 = new complex<double>[degree];
+
+	val1 = FFT(pol1, degree);
+	val2 = FFT(pol2, degree);
+
+	for (int i = 0; i < degree; i++)
+		val3[i] = val1[i] * val2[i];
+
+	pol3 = IFFT(val3, degree);
+
+	char * str3 = new char[degree];
+	
+	for (int i = 0; i < degree; i++)
+		str3[i] = (char)real(pol3[i]);
+
+	delete [] pol1;
+	delete [] pol2;
+	delete [] pol3;
+	delete [] val1;
+	delete [] val2;
+	delete [] val3;
+
+	return str3;
+}
 
 void addShiftsForChar( char c, char * str1n, char * str2n, int n, int * shift )
 {
@@ -26,13 +160,16 @@ void addShiftsForChar( char c, char * str1n, char * str2n, int n, int * shift )
 		swap(str2[i], str2[n - i - 1]);
 
 	char * str3 = new char[2 * n - 1];
-
+	/*
 	memset(str3, 0, (2 * n - 1) * sizeof(char));
 	
 	for (int i = 0; i < n; i++)
 		if (str2[n - i - 1] == 1)
 			for (int j = 0; j < n; j++)
 				str3[2 * n - 2 - (i + j)] += str1[n - j - 1];
+	*/
+
+	str3 = FastMultiply(str1, str2, n);
 
 	shift[0] += (int)str3[n - 1];
 	
@@ -50,7 +187,7 @@ void main()
 	char * str1n;
 	char * str2n;
 
-	//freopen("input.txt", "rt", stdin);
+	freopen("input.txt", "rt", stdin);
 	//freopen("output.txt", "wt", stdout);
 	
 	cin >> n;
