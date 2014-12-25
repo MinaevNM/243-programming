2153213b05c0a3a8866c3189c26253ebb4dd8f29
Nikita <nikita_340@mail.ru>
2014-12-25 09:18:55 +0300

    Triangulation finished.

--- 1.1/main.cpp
+++ 1.1/main.cpp
@@ -15,17 +15,12 @@ double length( point p1, point p2 )
 		return sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
 }
 
-double per( point p1, point p2, point p3 )
-{
-	return length(p1, p2) + length(p2, p3) + length(p3, p1);
-}
-
 void main()
 {
 	int n;
 
 	freopen("input.txt", "rt", stdin);
-	//freopen("output.txt", "wt", stdout);
+	freopen("output.txt", "wt", stdout);
 
 	cin >> n;
 
@@ -42,38 +37,31 @@ void main()
 		for (int j = 0; j < n; j++)
 			d[i][j] = 0;
 
-	for (int i = 0; i < n; i++)
-		for (int j = i + 1; j < n; j++)
-			d[i][j] = -1e10;
-
-	for (int i = 0; i < n; i++)
-		d[i][(i + 1) % n] = length(p[i], p[(i + 1) % n]);
-		
 	for (int i = 2; i < n; i++)
 		for (int j = 0; j < n; j++)
 		{
 			double min = 1e10;
 
-			for (int k = j; k < i + j; k++)
-				if (min < d[j][k] + d[k + 1][(i + j) % n] + per(p[(j - 1 + n) % n], p[k], p[(i + j + n) % n]))
-					min = d[j][k] + d[k + 1][(i + j) % n] + per(p[(j - 1 + n) % n], p[k], p[(i + j + n) % n]);
+			for (int k = j + 1; k < i + j; k++)
+			{
+				double len1, len2;
 
-			d[j][(i + j) % n] =  min;
+				len1 = length(p[j], p[k % n]);
+				len2 = length(p[k % n], p[(i + j) % n]);
+
+				if (k % n <= j + 1)
+					len1 = 0;
+				if ((i + j) % n <= k % n + 1)
+					len2 = 0;
+				
+				if (min > d[j][k % n] + d[k % n][(i + j) % n] + len1 + len2)
+					min = d[j][k % n] + d[k % n][(i + j) % n] + len1 + len2;
+			}
+
+			d[j][(i + j) % n] = min;
 		}
-	/*
-	for (int i = 0; i < n; i++)
-		for (int j = i + 1; j < n; j++)
-		{
-			double min = 1e10;
 
-			for (int k = i; k < j; k++)
-				if (min > d[i][k] + d[k + 1][j] + per(p[(i - 1 + n) % n], p[k], p[j]))
-					min = d[i][k] + d[k + 1][j] + per(p[(i - 1 + n) % n], p[k], p[j]);
-
-			d[i][j] = min;
-		}
-		*/
-	printf("%lf", d[0][n - 1]);
+	cout << d[0][n - 1] << endl;
 
 	for (int i = 0; i < n; i++)
 		delete [] d[i];
