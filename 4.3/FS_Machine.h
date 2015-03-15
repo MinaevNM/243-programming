f4a21f149746d62444a56ffeac2655c890275c41
Nikita <nikita_340@mail.ru>
2015-03-15 22:50:19 +0300

    Operations and Kleene added.

--- 4.3/FS_Machine.h
+++ 4.3/FS_Machine.h
@@ -11,7 +11,29 @@ class FS_Machine
 	set<char> ** matrix;
 
 public:
+	void print()
+	{
+		for (int i = 0; i < num_of_states; i++)
+		{
+			for (int j = 0; j < num_of_states; j++)
+			{
+				if (matrix[i][j].size() == 0)
+					cout << 0;
+				for (set<char>::iterator it = matrix[i][j].begin(); it != matrix[i][j].end(); it++)
+						cout << *it;
+				cout << ' ';
+			}
+			cout << endl;
+		}
+	}
+
+	FS_Machine();
 	FS_Machine( string regexp );
+	FS_Machine( char c );
+
+	static FS_Machine Operation( FS_Machine fsm1, FS_Machine fsm2, char op );
+	static FS_Machine Kleene( FS_Machine fsm );
+
 	bool check_string( string s );
 	void eps_closure();
 };
