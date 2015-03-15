68122fbfb0922cf3e9668b1b419e643b3a11699a
Nikita <nikita_340@mail.ru>
2015-03-15 22:51:15 +0300

    Operations and Kleene added.

--- 4.3/main.cpp
+++ 4.3/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <stack>
 
 #include "FS_Machine.h"
 
@@ -7,16 +8,62 @@ using namespace std;
 
 #pragma warning(disable: 4996)
 
+void sorting_station( string str )
+{
+	int length = str.size();
+	char * regexp = new char[length + 1];
+	strcpy(regexp, str.c_str());
+	int i = 0;
+	stack<char> s;
+
+	while (regexp[i])
+	{
+		if ((regexp[i] <= 'z' && regexp[i] >= 'a') || (regexp[i] <= 'Z' && regexp[i] >= 'A') || (regexp[i] >= '0' && regexp[i] <= '9') || regexp[i] == '@')
+			cout << regexp[i];
+		else if (regexp[i] == '(')
+			s.push(regexp[i]);
+		else if (regexp[i] == ')')
+		{
+			while (s.top() != '(')
+			{
+				cout << s.top();
+				s.pop();
+			}
+			s.pop();
+		}
+		i++;
+	}
+
+	cout << endl;
+}
+
 void main()
 {
 	int n;
 	string regexp;
 	string s;
 
+	FS_Machine f1('a');
+	FS_Machine f2('b');
+	FS_Machine f3('c');
+	FS_Machine f4('p');
+	FS_Machine f5('q');
+	FS_Machine f6;
+	FS_Machine f;
+	f = FS_Machine::Operation(f1, f2, '|');
+	f = FS_Machine::Kleene(f);
+	f = FS_Machine::Operation(f, f3, '&');
+	f6 = FS_Machine::Operation(f4, f5, '&');
+	f = FS_Machine::Operation(f, f6, '|');
+
 	freopen("input.cpp", "rt", stdin);
 	freopen("output.txt", "wt", stdout);
 
+	f.print();
+
 	cin >> regexp;
+
+	sorting_station(regexp);
 
 	FS_Machine * fsm = new FS_Machine(regexp);
 
