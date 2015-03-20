90963a7a41d8a643a9a66e1bc359b08b1e8234e8
Nikita <nikita_340@mail.ru>
2015-03-20 03:18:07 +0300

    Regexp parsing added.

--- 4.3/main.cpp
+++ 4.3/main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include <string>
 #include <stack>
+#include <queue>
 
 #include "FS_Machine.h"
 
 using namespace std;
 
 #pragma warning(disable: 4996)
+
+bool in_alphabet( char c )
+{
+	if ((c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A') || (c <= '9' && c >= '0'))
+		return true;
+	return false;
+}
 
 void sorting_station( string str )
 {
@@ -15,26 +23,106 @@ void sorting_station( string str )
 	strcpy(regexp, str.c_str());
 	int i = 0;
 	stack<char> s;
+	queue<char> q;
+	stack<FS_Machine> f;
+	FS_Machine f1, f2;
+	s.push(0);
+
+	cout << "SORTING STATION START" << endl;
 
 	while (regexp[i])
 	{
-		if ((regexp[i] <= 'z' && regexp[i] >= 'a') || (regexp[i] <= 'Z' && regexp[i] >= 'A') || (regexp[i] >= '0' && regexp[i] <= '9') || regexp[i] == '@')
-			cout << regexp[i];
-		else if (regexp[i] == '(')
-			s.push(regexp[i]);
-		else if (regexp[i] == ')')
+		switch (regexp[i])
 		{
+		case '(':
+			s.push(regexp[i]);
+			break;
+		case ')':
 			while (s.top() != '(')
 			{
-				cout << s.top();
+				q.push(s.top());
+				if (s.top() == '|' || s.top() == '&')
+				{
+					f1 = f.top();
+					f.pop();
+					f2 = f.top();
+					f.pop();
+					f.push(FS_Machine::Operation(f2, f1, s.top()));
+				}
+
 				s.pop();
 			}
 			s.pop();
+			break;
+		case '*':
+			f1 = f.top();
+			f.pop();
+			f1 = FS_Machine::Kleene(f1);
+			f.push(f1);
+			break;
+		case '&':
+			if (s.top() == '&')
+			{
+				q.push(s.top());
+				s.pop();
+				f1 = f.top();
+				f.pop();
+				f2 = f.top();
+				f.pop();
+				f.push(FS_Machine::Operation(f2, f1, s.top()));
+			}
+			s.push('&');
+			break;
+		case '|':
+			if (s.top() == '&' || s.top() == '|')
+			{
+				q.push(s.top());
+				f1 = f.top();
+				f.pop();
+				f2 = f.top();
+				f.pop();
+				f.push(FS_Machine::Operation(f2, f1, s.top()));
+				s.pop();
+			}
+			s.push('|');
+			break;
+		default:
+			q.push(regexp[i]);
+			f.push(FS_Machine(regexp[i]));
+			break;
 		}
+
 		i++;
 	}
 
-	cout << endl;
+	while (s.top())
+	{
+		f1 = f.top();
+		f.pop();
+		f2 = f.top();
+		f.pop();
+		f.push(FS_Machine::Operation(f2, f1, s.top()));
+		s.pop();
+	}
+
+	while (!f.empty())
+	{
+		f.top().print_list();
+		f.pop();
+		cout << endl << "---------------" << endl;
+	}
+
+	cout << "SORTING STATION FINISH" << endl;
+}
+
+void add_ampersand( string & s )
+{
+	for (int i = 0; i < s.size() - 1; i++)
+		if ((in_alphabet(s[i]) && in_alphabet(s[i + 1])) || (in_alphabet(s[i]) && s[i + 1] == '(') || (s[i] == ')' && in_alphabet(s[i + 1])) || (s[i] == '*' && (in_alphabet(s[i + 1]) || s[i + 1] == '(')))
+		{
+			s.insert(i + 1, "&");
+			i++;
+		}
 }
 
 void main()
@@ -59,9 +147,13 @@ void main()
 	freopen("input.cpp", "rt", stdin);
 	freopen("output.txt", "wt", stdout);
 
-	f.print();
+	f.print_matrix();
 
 	cin >> regexp;
+
+	add_ampersand(regexp);
+
+	cout << regexp << endl;
 
 	sorting_station(regexp);
 
